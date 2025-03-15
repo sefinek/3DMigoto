2f30b31958f7c749062cb2679c8bb6e5a399f0a4
Sefinek <contact@sefinek.net>
2025-03-15 02:11:25 +0100

    Update version.h

--- version.h
+++ version.h
@@ -28,7 +28,7 @@
 
 #define VERSION_MAJOR               3
 #define VERSION_MINOR               0
-#define VERSION_REVISION            0
+#define VERSION_REVISION            1
 
 #define STRINGIZE2(s) #s
 #define STRINGIZE(s) STRINGIZE2(s)
