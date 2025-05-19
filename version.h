9ddb30a0e9109a6fcdb7f8325fa8dcad1b957314
Sefinek <contact@sefinek.net>
2025-05-19 06:22:55 +0200

    Update

--- version.h
+++ version.h
@@ -28,17 +28,20 @@
 
 #define VERSION_MAJOR               3
 #define VERSION_MINOR               1
-#define VERSION_REVISION            1
+#define VERSION_REVISION            0
+#define VERSION_BUILD               1
 
 #define STRINGIZE2(s) #s
 #define STRINGIZE(s) STRINGIZE2(s)
 
-#define VER_FILE_VERSION            VERSION_MAJOR, VERSION_MINOR, VERSION_REVISION
-#define VER_FILE_VERSION_STR        STRINGIZE(VERSION_MAJOR)        \
-                                    "." STRINGIZE(VERSION_MINOR)    \
-                                    "." STRINGIZE(VERSION_REVISION) \
+#define VER_FILE_VERSION            VERSION_MAJOR, VERSION_MINOR, VERSION_REVISION, VERSION_BUILD
+#define VER_FILE_VERSION_STR \
+    STRINGIZE(VERSION_MAJOR) "." \
+    STRINGIZE(VERSION_MINOR) "." \
+    STRINGIZE(VERSION_REVISION) "." \
+    STRINGIZE(VERSION_BUILD)
 
-#define VER_PRODUCTNAME_STR         "3DMigoto"
+#define VER_PRODUCTNAME_STR         "3Dmigoto"
 #define VER_PRODUCT_VERSION         VER_FILE_VERSION
 #define VER_PRODUCT_VERSION_STR     VER_FILE_VERSION_STR
 #define VER_COPYRIGHT_STR           "Copyright (C) 2014-2025 by bo3b, Chiri, Sefinek."
